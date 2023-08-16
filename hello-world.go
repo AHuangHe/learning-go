9edb8757a6c14c303cb672ccb945bc2bbee1af82
Alberto Huang He <aahuanghe@gmail.com>
2023-08-15 19:26:24 -0700

    ah - Add if/else and switch lessons

--- hello-world.go
+++ hello-world.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"time"
 )
 
 const s string = "constant string"
@@ -76,6 +77,65 @@ func main() {
 		}
 		fmt.Println(n)
 	}
+
+	//________________________________________________________
+	// If/Else
+
+	// in Go, conditions don't need to be surrounded in parentheses
+	// if statements don't need an else statement
+	// a statement can precede conditionals, any variables declared are
+	// available in the current and all subsequent branches
+
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+
+	// no ternary if --> a ? b : c  (if a is true, then b, else c)
+	// need a full if statement even for basic conditions
+
+	//________________________________________________________
+	// Switch
+
+	// conditionals across many branches
+	// we can use commas to separate multiple expressions in the same case statement
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	case time.Monday:
+		fmt.Println("It's Monday :(")
+	default:
+		fmt.Println("It's a weekday")
+	}
+
+	// switch without an expression is an alternate way to express if/else logic
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	// a type switch compares types instead of values
+	// can be used to discover the type of an interface value
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
 
 	//________________________________________________________
 }
