bc3f83c8d60ac7d76a9292dab2fb7a8d8780aa9e
Alberto Huang He <aahuanghe@gmail.com>
2023-08-17 23:30:22 -0700

    ah - Add range lesson

--- hello-world.go
+++ hello-world.go
@@ -227,7 +227,6 @@ func main() {
 
 	//________________________________________________________
 	// Maps
-
 	// Go's built-in associative data type (hash/dicts)
 	// empty map -> make(map[key-type]val-type)
 	m := make(map[string]int)
@@ -259,5 +258,41 @@ func main() {
 	// initializing a map in a single line
 	m2 := map[string]int{"foo": 1, "bar": 2}
 	fmt.Println("map:", m2)
+	//________________________________________________________
+	// Range
+
+	// iterates over elements in a variety of data structures
+
+	nums := []int{2, 3, 4}
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	fmt.Println("sum:", sum)
+
+	// range on arrays and slices provides both index and value i, val := range array
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index:", i)
+		}
+	}
+
+	// range on map iterates over key-value pairs
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
+
+	// range can also iterate over just the keys of a map
+	for k := range kvs {
+		fmt.Println("key:", k)
+	}
+
+	// range on strings iterates over Unicode code points
+	// the first value is the starting byte index of the rune
+	// the second value is the rune itself
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
 	//________________________________________________________
 }
