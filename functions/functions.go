69354a4b567b88c14893027fccf4dc5e4429894d
Alberto Huang He <aahuanghe@gmail.com>
2023-08-18 01:20:57 -0700

    ah - Add recursion lesson

--- functions/functions.go
+++ functions/functions.go
@@ -58,6 +58,14 @@ func intSeq() func() int {
 }
 
 //________________________________________________________
+// Recursion
+
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n-1)
+}
 
 func main() {
 	res := plus(1, 2)
@@ -103,4 +111,21 @@ func main() {
 	newInts := intSeq()
 	fmt.Println(newInts())
 	//________________________________________________________
+	// Recursion
+
+	fmt.Println(fact(7))
+
+	// closures can also be recursive
+	// requires it to be declared with a typed var explicitly before it's defined
+	var fib func(n int) int
+
+	fib = func(n int) int {
+		if n < 2 {
+			return n
+		}
+
+		return fib(n-1) + fib(n-2)
+	}
+
+	fmt.Println(fib(7))
 }
